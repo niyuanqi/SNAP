f8273c7443528962012bb21ab95f68720457efba
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2017-03-21 15:38:55 -0400

    fixed LCgen

--- LCgen.py
+++ LCgen.py
@@ -140,7 +140,7 @@ for i in range(len(files)):
         if fo in fSs[b]:
             #source extractor did process image
             j = float(np.argwhere(fSs[b]==fo))
-            ts, RAs, DECs, Ms, Mserr = tSs[b][j], RASs[b][j], DECSs[b][j], MSs[b][j], MSs_err[b][j]
+            ts, RAs, DECs, Ms, Ms_err = tSs[b][j], RASs[b][j], DECSs[b][j], MSs[b][j], MSs_err[b][j]
             Stest = True
         else:
             #source extractor did not process image
