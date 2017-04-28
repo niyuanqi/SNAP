246463b9237c7203b928c9a41d595f451fffc45a
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2017-04-28 09:02:26 -0400

    Added to list of imports in init py

--- __init__.py
+++ __init__.py
@@ -2,5 +2,8 @@ import Analysis
 from Astrometry import *
 from Photometry import *
 from Catalog import *
+from Vizier import *
 from MagCalc import *
 from DiffIm import *
+from AutoSEx import *
+from MatchPhot import *
