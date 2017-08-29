437d0e784f300fa9a90feb5d5ad6a7d1172abb26
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2017-08-29 00:54:26 -0400

    kasen

--- Examples/KasenFit.py
+++ Examples/KasenFit.py
@@ -435,7 +435,7 @@ def test_a13(a13, sig):
                                                 wave_0[bands[band[i]]]],
                                      [m_c, e_51, z, 0],
                                      [m_c_err, e_51_err, zerr, t0err],
-                                     [10,10,10,10])
+                                     [1000,1000,1000,1000000])
             #print Fk[r], Fk_err[r]
         #for each angle
         for k, theta in enumerate(thetas):
