63aa6bc6ea5f583fae63731f05acb09e72bc2324
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2017-09-26 00:57:48 -0400

    kasen

--- Examples/KasenFit.py
+++ Examples/KasenFit.py
@@ -416,7 +416,7 @@ plt.show()
 print "Computing viewing angles at each separation distance"
 #list of sample models
 #a13s = np.arange(6.01,10.01,0.1) #1RG, 6MS, 2MS
-a13s = np.concatenate((np.arange(0.001,0.05,0.005), np.arange(0.05,0.2,0.05), np.arange(0.2, 2.0, 0.5), np.arange(2.0,11.0,2.0)))
+a13s = np.concatenate((np.arange(0.001,0.05,0.005), np.arange(0.05,0.2,0.02), np.arange(0.2, 2.0, 0.2), np.arange(2.0,11.0,1.0)))
 confs = [68.27, 99.54, 99.73]
 print [norm.ppf(conf/100.0) for conf in confs]
 print a13s
@@ -439,7 +439,7 @@ def test_a13(a13, sig):
                                                 wave_0[bands[band[i]]]],
                                      [m_c, e_51, z, 0],
                                      [m_c_err, e_51_err, zerr, t0err],
-                                     [1000000,1000000,1000000,1000000])
+                                     [100000,100000,100000,100000])
             #print Fk[r], Fk_err[r]
         #if i == 0:
             #print a13, max(Fk), Fk_err[np.argmax(Fk)]
