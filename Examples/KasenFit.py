0b55c16cd9f197b6ce9a24af3c704b8cef22de90
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2017-09-29 00:45:09 -0400

    kasen

--- Examples/KasenFit.py
+++ Examples/KasenFit.py
@@ -415,7 +415,7 @@ print "Computing viewing angles at each separation distance"
 #list of sample models
 #a13s = np.arange(6.01,10.01,0.1) #1RG, 6MS, 2MS
 a13s = np.concatenate((np.arange(0.001,0.05,0.005), np.arange(0.05,0.2,0.02), np.arange(0.2, 2.0, 0.05), np.arange(2.0,11.0,1.0)))
-confs = [68.27, 99.54, 99.73]
+confs = [68.27, 80.0, 90.0, 99.54, 99.73]
 print [norm.ppf(conf/100.0) for conf in confs]
 print a13s
 #list of viewing angles
