444984343a94094fe715e7989600745ad4024784
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2018-07-17 16:45:26 -0400

    Added missing error for stars which leak off the edge

--- Photometry.py
+++ Photometry.py
@@ -199,7 +199,7 @@ def PSFextract(image, x0, y0, fwhm=5.0, fitsky=True, sat=40000.0, verbosity=0):
     #get fit box to fit psf
     fsize = 1
     intens, x, y = ap_get(image, x0, y0, 0, fsize*fwhm)
-    if len(intens) == 0:
+    if len(intens) == 0 or intens.sum() == 0:
         raise MissingError('Ref star at ('+str(x0)+','+str(y0)+') not in image')
     #get an approximate fix on position
     x0 = np.sum(intens*x)/intens.sum()
