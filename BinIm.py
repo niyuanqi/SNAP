648f22be91aa01412c69de3ff30eaaf55452f9d2
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2018-11-03 11:53:46 -0400

    Made BinIm useable

--- BinIm.py
+++ BinIm.py
@@ -52,8 +52,9 @@ if __name__ == "__main__":
     parser.add_argument('band', type=str, help='band to bin.')
     parser.add_argument('t1', type=float, help='start time, day of year float')
     parser.add_argument('t2', type=float, help='end time, day of year float')
+    parser.add_argument('year', type=float, help='year number float')
     parser.add_argument('out_name', type=str, help='output binned file name')
     args = parser.parse_args()
     
     #create binned image
-    binTimes(args.band, args.t1, args.t2, args.out_name)
+    binTimes(args.band, args.t1, args.t2, args.year, args.out_name)
