2f2a8842fcb4eade0eb485d189f2d4423af393e0
Yuan Qi Ni <chris.ni@mail.utoronto.ca>
2019-01-11 17:31:59 -0500

    Improved AAVSO B-V correlation plotting

--- MagPlot.py
+++ MagPlot.py
@@ -110,7 +110,7 @@ def col_corr(cat, catname, catIDs, RAo, DECo, radius, insMags, insMagerrs, catMa
     #fit color dependence
     plt.title("B band dependence on B-V")
     plt.errorbar(BV, dI, xerr=BV_err, yerr=dI_err, fmt='r+', zorder=1)
-    popt, pcov = curve_fit(linfunc,BV,dI,p0=[0.27,27.8],sigma=dI_err)
+    popt, pcov = curve_fit(linfunc,BV,dI,p0=[0.27,27.8])
     perr = np.sqrt(np.diag(pcov))
     colsol = linfunc(BV, *popt)
     print "Color correlation:",popt, perr
